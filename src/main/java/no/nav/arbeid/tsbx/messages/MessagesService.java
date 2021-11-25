a8a9d7f8ce34327da46f7eccde64d590a6ff03ad
Øyvind Stegard <oyvind.stegard@nav.no>
2021-11-25 10:26:08 +0100

    Cleanup

--- src/main/java/no/nav/arbeid/tsbx/messages/MessagesService.java
+++ src/main/java/no/nav/arbeid/tsbx/messages/MessagesService.java
@@ -23,13 +23,16 @@ public class MessagesService {
                 "Danger Delivery AS",
                 "Norsk it AS",
                 "Vi ringer DU bringer ANS",
-                "Ditt Renhold AS"
+                "Ditt Renhold AS",
+                "Kulsveen Logistikk AS",
+                "Kulsveen Transport AS"
         };
         final String[] texts = {
                 "Kan du dele din CV med oss ?",
                 "Hei, vi har en viktig melding til deg. Du er utvalgt.",
                 "Vi har en bra annonse ute nå, sjekk den ut !",
-                "Kan du begynne i dag ? Ring oss på 22222222 .."
+                "Kan du begynne i dag ? Ring oss på 22222222 ..",
+                "Invitasjon til jobbtreff: ..."
         };
 
         final Random rng = new Random(user.pid().hashCode());
@@ -37,9 +40,9 @@ public class MessagesService {
         final int nMessages = rng.nextInt(Integer.MAX_VALUE) % 5 + 1;
 
         for (int i=0; i<nMessages; i++) {
-            final String title = titles[rng.nextInt(Integer.MAX_VALUE) % titles.length];
-            final String employer = employers[rng.nextInt(Integer.MAX_VALUE) % employers.length];
-            final String text = texts[rng.nextInt(Integer.MAX_VALUE) % texts.length];
+            final String title = titles[rng.nextInt(titles.length)];
+            final String employer = employers[rng.nextInt(employers.length)];
+            final String text = texts[rng.nextInt(texts.length)];
             messages.add(new Message(title.formatted(user.name()), employer, text));
         }
 
